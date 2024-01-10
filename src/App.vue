3a5728b28b2dac65f9eb9c6ce6d6c958034a76ca
Henry Ge <1981761687@qq.com>
2024-01-10 16:44:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <img src="../src/assets/background-image.jpg" class="background-image">
+  <div class="content"></div>
+  <RouterView />
+</template>
+
+<style scoped>
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+.content {
+  background-color: white;
+  margin-top: 60vh;
+  width: 100%;
+  height: 100vh;
+}
+</style>
