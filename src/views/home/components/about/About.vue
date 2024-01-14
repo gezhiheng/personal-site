e5a10da4436e8dd067b8c7952fa177e0fb69e228
Henry Ge <1981761687@qq.com>
2024-01-14 22:48:00 +0800

    refactor：更新组件样式

--- src/views/home/components/about/About.vue
+++ src/views/home/components/about/About.vue
@@ -1,17 +1,33 @@
 <script setup>
+import RenderMarkdown from '@/components/RenderMarkdown'
+import atricle from '../../../../assets/docs/Elementplus的el-input获取焦点.md?raw'
 
+const text = '# test\nThis is a test\n```vue\n  code \n```'
 </script>
 
-<template #about>
+<template>
   <div class="about">
     <h1>关于我</h1>
+    <RenderMarkdown class="content" :markdownText="atricle" />
   </div>
 </template>
 
 <style scoped>
 .about {
-  background-color: red;
+  background-color: #ffffff;
   height: 100vh;
   width: 100%;
+}
+
+.content {
+  display: block;
+  margin: 0 auto;
+  width: 60%;
+}
+
+@media screen and (max-width: 992px) {
+  .content {
+    width: 90%;
+  }
 }
 </style>
