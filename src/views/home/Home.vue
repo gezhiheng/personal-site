da9738657114e62613c5554cdfb6d2587ac7bd59
Henry Ge <1981761687@qq.com>
2024-03-04 17:27:08 +0800

    refactor: 删除文件引用的代码

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Menu from '@/components/Menu'
 import Header from './components/Header'
-import About from './components/about'
 import Blog from './components/blog'
 import Contact from './components/contact'
 
