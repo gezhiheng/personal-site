a5012ad443b62188f10fb36279397bc910ca0e16
Henry Ge <1981761687@qq.com>
2024-03-04 17:28:42 +0800

    refactor: 更改打包时的配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import './style.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import Vue3Lottie from 'vue3-lottie'
 import router from './router'
 
-const app = createApp(App)
+import './style.css'
 
+const app = createApp(App)
 app.use(Vue3Lottie)
 app.use(router)
 app.mount('#app')
+console.log('main.js')
