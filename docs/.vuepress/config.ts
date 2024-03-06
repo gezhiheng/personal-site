2b8cb3be049a4e18994884ea26359cc3daa85364
Henry Ge <1981761687@qq.com>
2024-03-06 17:16:29 +0800

    refactor: 更改docs打包配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,10 +3,11 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/docs/",
-  head: [['link', { rel: 'icon', href: 'https://my-pic-base.oss-cn-beijing.aliyuncs.com/avatar.jpg' }]],
+  dest: "./dist/docs",
+  head: [["link", { rel: "icon", href: "https://my-pic-base.oss-cn-beijing.aliyuncs.com/avatar.jpg" }]],
   lang: "zh-CN",
-  title: "葛智恒的博客",
-  description: "葛智恒的博客",
+  title: "",
+  description: "",
   theme,
 
   // 和 PWA 一起启用
